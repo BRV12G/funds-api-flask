7795ab1433d4f15feac71babbb0be6a89151f93e
Bhairavi <bhairavigawas9@gmail.com>
2025-10-23 10:56:37 +0530

    feat: route for funds

--- models.py
+++ models.py
@@ -9,11 +9,28 @@ class Users(db.Model):
     password = db.Column(db.string(250), nullable=False)
     email = db.Column(db.string(100), unique=True, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    funds = db.relationship('Funds', backref='user')
 
+)
 
     def __repr__(self): #output in python shell or logs , the __repr__ function.
         return f'<User {self.firstNamee} {self.id}>'
 
 
-class 
+class Funds(db.Model):
+    __tablename__ = "Funds"
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    # name = db.Column(db.string(100), nullable=False)
+
+    @property #
+    def serialize(self): # serialize converts the SQLAlchemy object → Python dictionary → JSON-friendly.
+
+        return {
+            'id': self.id,
+            'amount': self.amount,
+            'created_at': self.created_at,
+        }
 
